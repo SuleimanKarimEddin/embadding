104ec1cd84384d0ecb27c5a5365509663c79a3ee
SuleimanKarimEddin <157278692+SuleimanKarimEddin@users.noreply.github.com>
2024-02-07 17:42:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import { pipeline } from "@sroussey/transformers";
 import express from "express";
 import cors from "cors";
-
+import bodyParser from "body-parser";
 const app = express();
 app.use(cors());
-const PORT = 3000;
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+const PORT = 3005;
 const extractor = await pipeline(
   "feature-extraction",
   "Xenova/all-MiniLM-L6-v2"
@@ -14,12 +18,28 @@ app.get("/", async (req, res) => {
   if (typeof req.query.word !== "string")
     return res.send("Word is not a string");
   const word = req.query.word;
+
   const output = await extractor(word, { pooling: "mean", normalize: true });
   const dataArray = Array.from(output.data.values());
   const data = {
     data: dataArray,
   };
-  res.send(data);
+  return res.send(data);
+});
+
+app.post("/list", async (req, res) => {
+  if (!Array.isArray(req.body))
+    return res.send("Word is not an array of strings");
+  const word = req.body;
+        console.log("start");
+
+  const output = await extractor(word, { pooling: "mean", normalize: true });
+  const data = {
+    data: output.tolist(),
+  };
+        console.log("finish");
+
+  return res.send(data);
 });
 
 app.listen(PORT, () => {
